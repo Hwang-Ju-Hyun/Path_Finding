4dc074965961adfee674a8ded52131b844f950f1
HwangJuHyun <hyunhwang091@naver.com>
2024-11-17 18:24:53 +0900

    dfn

--- Source/movement_student.cpp
+++ Source/movement_student.cpp
@@ -67,7 +67,7 @@ void DrawPath(const std::vector<Node>& _path, DebugDrawingColor _eColor)
 
 void Movement::DrawParentPathAndMove(const std::vector<std::pair<int, int>>& _path)
 {
-	for (int i = _path.size() - 1; i > 0; i--)
+	for (int i = _path.size() - 1; i >=0; i--)
 	{
 		D3DXVECTOR3 spot = g_terrain.GetCoordinates(_path[i].first, _path[i].second);
 		m_waypointList.push_back(spot);
@@ -75,30 +75,31 @@ void Movement::DrawParentPathAndMove(const std::vector<std::pair<int, int>>& _pa
 	}	
 }
 
-bool IsLineClear(int startRow, int startCol, int endRow, int endCol)
-{
-    // Bresenham's Line Algorithm or similar technique to check for obstacles in a straight line
+//브레젠험 알고리즘? 씨발 몰라서 걍 블로그 그대로 뺏김
+//Todo: err변수랑 하는일 이해하기
+bool GoStraight(int startRow, int startCol, int endRow, int endCol)
+{    
     int delta_col = abs(endCol - startCol);
     int delta_row = abs(endRow - startRow);
     int col = (startCol < endCol) ? 1 : -1;
     int row = (startRow < endRow) ? 1 : -1;
     int err = delta_col - delta_row;
 
-    // Determine the number of steps required based on the largest dimension (dx or dy)
+
     int maxSteps = max(delta_col, delta_row);
 
-    // Use a for loop to iterate through each step from the start to the end
+
     for (int i = 0; i <= maxSteps; i++)
     {
-        // If there's a wall at the current point, return false
+
         if (g_terrain.IsWall(startRow, startCol))
             return false;
 
-        // If we've reached the end point, break out of the loop
+
         if (startRow == endRow && startCol == endCol)
             break;
 
-        // Update the error term and adjust the row/column as necessary
+        
         int e2 = err * 2;
 
         if (e2 > -delta_row)
@@ -132,10 +133,81 @@ void erase_element(std::vector<std::pair<int, int>>* _vec,int _idx)
 }
 
 
-bool Movement::ComputePath(int r, int c, bool newRequest)
+void SmoothPath(std::vector<std::pair<int, int>>& _path)
 {
+    if (_path.size() < 3)
+        return;
+
+    std::vector<std::pair<int, int>> smoothPath;
+    smoothPath.push_back(_path[0]);
+
+    for (size_t i = 1; i < _path.size() - 1; ++i)
+    {
+        auto previous_point = _path[i - 1];
+        auto current_point = _path[i];
+        auto next_point = _path[i + 1];
+
+        float newX = (previous_point.first + current_point.first + next_point.first) / 3.0f;
+        float newY = (previous_point.second + current_point.second + next_point.second) / 3.0f;
+
+        smoothPath.push_back({(int)newX,(int)newY });
+    }
+
+    smoothPath.push_back(_path[_path.size() - 1]);
+    _path = smoothPath;
+}
+
+std::vector<D3DXVECTOR3> CatmullRom(const std::vector<D3DXVECTOR3>& _points, int _segment)
+{
+    std::vector<D3DXVECTOR3> smoothPth;
+
+    if (_points.size() < 4)
+    {
+        
+        return smoothPth;
+    }
+
+    for (size_t i = 1; i < _points.size() - 2; i++)
+    {
+        D3DXVECTOR3 p0 = _points[i - 1];
+        D3DXVECTOR3 p1 = _points[i];
+        D3DXVECTOR3 p2 = _points[i + 1];
+        D3DXVECTOR3 p3 = _points[i + 2];
+
+        for (int j = 0; j <= _segment; j++)
+        {
+            float t = float(j) / _segment;
+            D3DXVECTOR3 point;
+            D3DXVec3CatmullRom(&point, &p0, &p1, &p2, &p3, t);
+            smoothPth.push_back(point);
+        }
+    }
+
+    return smoothPth;
+}
+
+
+std::vector<D3DXVECTOR3> SmoothPathWithCatmullRom(const std::vector<std::pair<int, int>>& _path, int _segment)
+{
+    std::vector<D3DXVECTOR3> controlPoints;
+    std::vector<D3DXVECTOR3> smoothedPath;
+
+    for (int i = 0; i < _path.size(); i++)
+    {
+        controlPoints.push_back(g_terrain.GetCoordinates(_path[i].first, _path[i].second));
+    }   
+    smoothedPath = CatmullRom(controlPoints, _segment);
+    return smoothedPath;
+}
+
+
+
+bool Movement::ComputePath(int r, int c, bool newRequest)
+{            
     m_goal = g_terrain.GetCoordinates(r, c);
     m_movementMode = MOVEMENT_WAYPOINT_LIST;
+
+    std::cout <<r<<","<<c << std::endl;
 
     int curR, curC;
     D3DXVECTOR3 cur = m_owner->GetBody().GetPos();
@@ -174,7 +246,7 @@ bool Movement::ComputePath(int r, int c, bool newRequest)
         start_Node.col = start_Col;
         start_Node.gx = 0;
         start_Node.parent = { start_Node.row, start_Node.col };
-        start_Node.hx = 0;//GetHeuristic(start_Row,start_Col,target_Row,target_Col,HEURISTIC::MANHATTAN);
+        start_Node.hx = GetHeuristic(start_Row,start_Col,target_Row,target_Col,HEURISTIC::MANHATTAN);
         start_Node.fx = start_Node.gx + start_Node.hx;
 
         close_grid[start_Row][start_Col] = true;
@@ -183,8 +255,7 @@ bool Movement::ComputePath(int r, int c, bool newRequest)
         
         if (m_straightline)
         {
-
-            if (IsLineClear(cur_Row, cur_Col, target_Row, target_Col))
+            if (GoStraight(cur_Row, cur_Col, target_Row, target_Col))
             {
                 D3DXVECTOR3 spot = { target.x, target.y, target.z };
                 m_waypointList.push_back(spot);
@@ -195,40 +266,37 @@ bool Movement::ComputePath(int r, int c, bool newRequest)
         while (!open_list.empty())
         {
             Node add_node;
-            int direct_Row[8] = { 1, 0, -1, 0, 1, -1, -1, 1 };
-            int direct_Col[8] = { 0, 1, 0, -1, 1, 1, -1, -1 };
+            //                   ↑(0)   →(1)   ↓(2)   ←(3)  ↗(4)   ↘(5)  ↙(6)  ↖(7)
+            int direct_Row[8] = { 1,     0,    -1,     0,    1,     -1,    -1,     1 };
+            int direct_Col[8] = { 0,     1,     0,    -1,    1,      1,    -1,    -1 };
 
             int cur_Row = open_list.top().row;
             int cur_Col = open_list.top().col;
             int cur_gx = open_list.top().gx;
-            close_list.push_back(open_list.top());
+            close_list.push_back(open_list.top());            
             open_list.pop();
-
             if (cur_Row == r && cur_Col == c)
-            {
+            {                
                 int parent_row = close_list[close_list.size() - 1].row;
                 int parent_col = close_list[close_list.size() - 1].col;
+                result_path.push_back({ close_list[close_list.size() - 1].row, close_list[close_list.size() - 1].col });
                 for (int i = close_list.size() - 1; i > 0; i--)
                 {
                     if (parent_row == close_list[i].row && parent_col == close_list[i].col)
                     {
                         parent_row = close_list[i].parent.first;
-                        parent_col = close_list[i].parent.second;
-                        if (parent_row == start_Node.row && parent_col == start_Node.col)
-                        {
-                            result_path.push_back({ parent_row, parent_col });
-                            break;
-                        }
+                        parent_col = close_list[i].parent.second;                        
                         result_path.push_back({ parent_row, parent_col });
-                    }   
-                }       
+                    }
+                }
+                
                 std::vector<std::pair<int, int>> rubberband_path;
                 if (m_rubberband)
                 {                    
                     for(int i = 0; i < result_path.size(); i++)                    
                         rubberband_path.push_back({ result_path[i].first,result_path[i].second });
                         
-                    for (int i = rubberband_path.size()-1; i >0; i--)
+                    for (int i = rubberband_path.size()-1; i >=0; i--)
                     {    
                         if (i <= 2)
                         {                            
@@ -240,7 +308,7 @@ bool Movement::ComputePath(int r, int c, bool newRequest)
                         int n2_col = rubberband_path[i - 1].second;
                         int n3_row = rubberband_path[i - 2].first;
                         int n3_col = rubberband_path[i - 2].second;
-                        if (IsLineClear(n1_row,n1_col,n3_row,n3_col))
+                        if (GoStraight(n1_row,n1_col,n3_row,n3_col))
                         {
                             erase_element(&rubberband_path, i-1);
                         }
@@ -249,21 +317,33 @@ bool Movement::ComputePath(int r, int c, bool newRequest)
                             continue;
                         }                        
                     }
-                }
-                if (m_rubberband)
+                }             
+                std::vector<D3DXVECTOR3> smooth_path;    
+                
+                DrawPath(blue_list, DEBUG_COLOR_BLUE);
+                DrawPath(close_list, DEBUG_COLOR_YELLOW);
+
+                if (m_smooth)
                 {
-                    DrawParentPathAndMove(rubberband_path);                    
+                    smooth_path = SmoothPathWithCatmullRom(result_path, 10);
+                    for (int i = smooth_path.size() - 1; i >=0; i--)
+                    {
+                        D3DXVECTOR3 spot = { smooth_path[i].x, smooth_path[i].y,smooth_path[i].z };
+                        m_waypointList.push_back(spot);                            
+                    }                    
+                }
+                else if (m_rubberband)
+                {
+                    DrawParentPathAndMove(rubberband_path);                         
                 }
                 else
-                {
-                    DrawPath(blue_list, DEBUG_COLOR_BLUE);
-                    DrawPath(close_list, DEBUG_COLOR_YELLOW);
+                {                                    
                     DrawParentPathAndMove(result_path);
                     result_path.clear();
                 }                
                 return true;
             }
-
+            
             for (int i = 0; i < 8; i++)
             {
                 int next_Row = cur_Row + direct_Row[i];
@@ -278,7 +358,7 @@ bool Movement::ComputePath(int r, int c, bool newRequest)
                 if (close_grid[next_Row][next_Col])
                     continue;
 
-                if (i >= 4)
+                if (i >= 4)//i>=4이상이면 대각선이라는 뜻
                 {
                     int adj_Row1 = cur_Row + direct_Row[i];
                     int adj_Col1 = cur_Col;
@@ -299,7 +379,7 @@ bool Movement::ComputePath(int r, int c, bool newRequest)
                 {
                     add_node.gx = cur_gx + 14;
                 }
-                add_node.hx = 0;//(abs(target_Row - next_Row) + abs(target_Col - next_Col)) * 10;
+                add_node.hx = (abs(target_Row - next_Row) + abs(target_Col - next_Col)) * 10;
                 add_node.fx = add_node.gx + add_node.hx;
 
                 add_node.parent = { cur_Row, cur_Col };
